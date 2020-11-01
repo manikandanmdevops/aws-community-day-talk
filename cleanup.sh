966594dc78988dbb82e41b9b9810192387325603
manikandanmdevops <71846327+manikandanmdevops@users.noreply.github.com>
2020-11-01 18:41:53 +0530

    commiting

--- cleanup.sh
+++ cleanup.sh
@@ -2,4 +2,4 @@
 docker container stop $(docker container ps -a -q) 2>&1 && echo "All containers stopped" || echo "No containers found to stop"
 docker container rm $(docker container ps -a -q) 2>&1 && echo "All containers removed" || echo "No containers found to remove"
 
-docker image rm yashvardhan-kukreja/aws-community-day:test 2>&1 && echo "Removed the test image" || echo "Test image not found"
+docker image rm manikandanmdevops/aws-community-day:test 2>&1 && echo "Removed the test image" || echo "Test image not found"

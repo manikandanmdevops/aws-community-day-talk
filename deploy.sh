83d58312999fdf11927fa5b1460cc0d98aeeaff2
manikandanmdevops <71846327+manikandanmdevops@users.noreply.github.com>
2020-11-02 14:46:30 +0530

    test111

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cd ~/
 sudo rm -rf aws-community-day-talk
-git clone git@github.com:manikandanmdevops/aws-community-day-talk.git
+git clone https://github.com/manikandanmdevops/aws-community-day-talk.git
 cd aws-community-day-talk
 kubectl apply -f deployment.yaml
